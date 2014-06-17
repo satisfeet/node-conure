eb12b655ba32d905b22976f77b60262d66c742ad
Bodo Kaiser <i@bodokaiser.io>
2014-06-17 10:42:36 +0200

    replaced cogent with custom https.request

--- opt/test/index.js
+++ opt/test/index.js
@@ -1,6 +1,9 @@
 var co     = require('co');
 var chai   = require('chai');
+var cogent = require('cogent');
 var conure = require('../../lib');
+
+const URL = 'https://engine.satisfeet.me';
 
 const USERNAME = process.env.USERNAME;
 const PASSWORD = process.env.PASSWORD;
@@ -14,6 +17,12 @@ describe('createClient(options)', function() {
         password: 'bar'
       });
     });
+  });
+
+  it('should throw error on missing "options"', function() {
+    chai.expect(function() {
+      conure.createClient();
+    }).to.throwError;
   });
 
   it('should throw error on missing "username"', function() {
@@ -31,21 +40,6 @@ describe('createClient(options)', function() {
   describe('Class: Client', function() {
 
     describe('#find(name[, query])', function() {
-
-      it('should throw error', function(done) {
-        var client = conure.createClient({
-          username: 'foo',
-          password: 'bar'
-        });
-
-        co(function* () {
-          yield client.find('customers');
-        })(function(err) {
-          chai.expect(err).to.be.an.instanceOf(Error);
-
-          done();
-        });
-      });
 
       it('should return array', function(done) {
         var client = conure.createClient({
@@ -65,23 +59,111 @@ describe('createClient(options)', function() {
         });
       });
 
-    });
-
-    describe('#findOne(name, query)', function() {
-
-      it('should throw error', function(done) {
+      it('should throw error on missing "name"', function(done) {
         var client = conure.createClient({
-          username: 'foo',
-          password: 'bar'
+          username: USERNAME,
+          password: PASSWORD
         });
 
         co(function* () {
-          yield client.findOne('customers', { id: '1234' });
+          yield client.find();
         })(function(err) {
           chai.expect(err).to.be.an.instanceOf(Error);
 
           done();
         });
+      });
+
+      it('should throw error on invalid credentials', function(done) {
+	var client = conure.createClient({
+	  username: 'foo',
+	  password: 'bar'
+	});
+
+	co(function* () {
+	  yield client.findOne('customers', {
+	    id: 123
+	  });
+	})(function(err) {
+	  chai.expect(err)
+	    .to.be.an.instanceOf(Error)
+	    .to.have.property('name')
+	    .to.contain('Authentication');
+
+	  done();
+	});
+      });
+
+    });
+
+    describe('#findOne(name, query)', function() {
+
+      it('should not throw error', function(done) {
+        var client = conure.createClient({
+          username: USERNAME,
+          password: PASSWORD
+        });
+
+        co(function* () {
+          return yield client.findOne('customers', {
+	    id: '1234'
+	  });
+        })(function(err, body) {
+          chai.expect(err).to.not.exist;
+	  chai.expect(body).to.be.null;
+
+          done();
+        });
+      });
+
+      it('should throw error on missing "name"', function(done) {
+        var client = conure.createClient({
+          username: USERNAME,
+          password: PASSWORD
+        });
+
+        co(function* () {
+          yield client.findOne();
+        })(function(err) {
+          chai.expect(err).to.be.an.instanceOf(Error);
+
+          done();
+        });
+      });
+
+      it('should throw error on missing "query"', function(done) {
+        var client = conure.createClient({
+          username: USERNAME,
+          password: PASSWORD
+        });
+
+        co(function* () {
+          yield client.findOne('customers');
+        })(function(err) {
+          chai.expect(err).to.be.an.instanceOf(Error);
+
+          done();
+        });
+      });
+
+      it('should throw error on invalid credentials', function(done) {
+	var client = conure.createClient({
+	  username: 'foo',
+	  password: 'bar'
+	});
+
+	co(function* () {
+	  yield client.findOne('customers', {
+	    id: 123
+	  });
+	})(function(err) {
+	  chai.expect(err)
+	    .to.be.an.instanceOf(Error)
+	    .to.have.property('name')
+	    .to.contain('Authentication');
+
+	  done();
+	});
       });
 
     });
